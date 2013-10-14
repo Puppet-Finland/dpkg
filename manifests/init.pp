a2d3918aacd0afabba21105a39318715658c7c1f
Samuli Seppänen <samuli@openvpn.net>
2013-10-14 14:54:48 +0300

    Initial import to GitHub
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

---
+++ manifests/init.pp
@@ -0,0 +1,19 @@
+#
+# == Class: dpkg
+#
+# This class is currently just a dummy class. All of the interesting stuff is 
+# and probably will be in the various subclasses.
+#
+# == Authors
+#
+# Samuli Seppänen <samuli@openvpn.net>
+#
+# == License
+#
+# BSD-lisence
+# See file LICENSE for details
+#
+class dpkg {
+
+    # This class is empty at the moment
+}
